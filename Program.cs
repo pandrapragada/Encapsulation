bf556e998bbb6fbc390411f8bf51745129d8309f
Pandrapragada Himabindhu <Pandrapragada.Himabindhu@ibm.com>
2022-03-07 08:42:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+            namespace RectangleApplication
+    {
+        class Rectangle
+        {
+            private double length;
+            private double width;
+
+            public void Acceptdetails()
+            {
+                length = 10;
+                width = 15;
+            }
+
+            public double GetArea()
+            {
+                return length * width;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine("Length: {0}", length);
+                Console.WriteLine("Width: {0}", width);
+                Console.WriteLine("Area: {0}", GetArea());
+            }
+        }
+
+        class ExecuteRectangle
+        {
+            static void Main(string[] args)
+            {
+                Rectangle r = new Rectangle();
+                r.Acceptdetails();
+                r.Display();
+                Console.ReadLine();
+            }
+        }
+    }
+
